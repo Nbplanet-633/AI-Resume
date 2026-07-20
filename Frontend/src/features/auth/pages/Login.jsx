99d9b0aa546e6c8ad81a36f36bdf09a43b45ed97
Nbplanet-633 <njmeena633725@gmail.com>
2026-07-20 12:53:54 +0530

    restructure project into backend and frontend and added ui to login and register page

---
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "../auth.form.scss";
+import { useNavigate, Link } from "react-router";
+
+const Login = () => {
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <main>
+      <div className="form-container">
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label htmlFor="email">email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Enter your email address"
+            ></input>
+          </div>
+
+          <div className="input-group">
+            <label htmlFor="password">password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Enter your email password"
+            ></input>
+          </div>
+
+          <button className="button primary-button">Login</button>
+        </form>
+
+        <p>
+          {" "}
+          Don't have an account? <Link to={"/register"}> Register</Link>
+        </p>
+      </div>
+    </main>
+  );
+};
+
+export default Login;
